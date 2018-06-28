0b3668be5fe9c0e49497e2f27592cab158f9555c
Charlie Chin <cchin013@ucr.edu>
2018-06-27 17:49:32 -0700

    steam review scraper implemented
    function getAllGameReviews now properly goes through all games on Steam and gets their reviews
    
    also added a csv file for saving the data

--- main.py
+++ main.py
@@ -24,75 +24,74 @@ from pyspark.sql import SparkSession
 # soup = bSoup(htmlSite, 'html.parser')
 # print(soup)
 
-#Purpose: Get all steam reviews for the dataset we give to the function
+#Purpose: Get all steam reviews for the dataset of game titles we give to the function
 #Input: some dataset with the steam games we want analyzed
+#Result: reviewList now filled with all reviews for their corresponding game titles
 #Returns: None
 def getAllGameReviews(dataset):
-
     #Steam API parameters for what we want: all recent reviews(including positive and negative) in English in the last 10 days, purchased from anywhere
     #More info here: https://partner.steamgames.com/doc/store/getreviews
     parameters = {'filter': 'recent', 'language': 'english', 'day_range': '10', 'start_offset': '0',
                   'review_type': 'all',
                   'purchase_type': 'all'}
     #Go through all games in the database
-    print(reviewList)
-    for i in range(len(dataset["games"])):
+    for i in range(len(appNameAndID["applist"]["apps"])):
         # Reset offset to 0 after every game
         parameters["start_offset"] = 0
-
         # Get a new game to analyze
         response = requests.get(
-            'https://store.steampowered.com/appreviews/' + str(dataset["games"][i]["appid"]) + '?json=1',
+            'https://store.steampowered.com/appreviews/' + str(appNameAndID["applist"]["apps"][i]["appid"]) + '?json=1',
             params=parameters)
         reviewsResponse = response.json()
-
         # If the query is empty, skip that game/app
         if reviewsResponse["success"] == 2:
             continue
-        print("_____________________________THE FOLLOWING IS FOR THE GAME: " + str(dataset["games"][i]["title"]))
+        print("Looking through reviews for game: " + str(appNameAndID["applist"]["apps"][i]["name"]))
+        #print("_____________________________THE FOLLOWING IS FOR THE GAME: " + str(dataset["games"][i]["title"]))
         # Go through all the reviews for a given game
         for j in range(reviewsResponse["query_summary"]["total_reviews"]):
-
             # Go through all reviews for the game in batches of 20
             # Place them in the proper location in our reviewList dataset based on the game
             for k in range(len(reviewsResponse["reviews"])):
                 #debugging statement to check if we are really getting reviews
                 #print(reviewsResponse["reviews"][k]["review"])
-                reviewList["titles"][dataset["games"][i]["title"]]["reviews"].append({
-                    reviewsResponse["reviews"]
+                reviewList["titles"][appNameAndID["applist"]["apps"][i]["name"]]["reviews"].append({
+                    reviewsResponse["reviews"][k]["review"]
                 })
             # Get the next batch of 20, e.g. first we get reviews 0-20, then 21-40, etc..
             parameters["start_offset"] += 20
             response = requests.get(
-                'https://store.steampowered.com/appreviews/' + str(dataset["games"][i]["appid"]) + '?json=1',
+                'https://store.steampowered.com/appreviews/' + str(appNameAndID["applist"]["apps"][i]["appid"]) + '?json=1',
                 params=parameters)
             reviewsResponse = response.json()
-            j += 20
+            j += 19
 #Get a list of all games on Steam
 response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
 appNameAndID = response.json()
-dataset = {
-    'games': [{
-        'title': '',
-        'appid': ''
-    }]
-}
+
+#key: game name, value: game's app id, e.g. dataset["Counter-Strike"] returns 10 (its appid)
+dataset = {}
+#key: "titles"->game name->reviews, value: all reviews for that game in an arbitrary order
 reviewList = {
-    'titles': [{
-    }]
+    'titles': {
+    }
 }
+
 #Populate our dataset with all available apps/games on Steam
 for i in range(len(appNameAndID["applist"]["apps"])):
-    dataset["games"].append( {
-        'title': appNameAndID["applist"]["apps"][i]["name"],
-        'appid': appNameAndID["applist"]["apps"][i]["appid"]
-    }
-    )
-    reviewList["titles"].append( {
-        appNameAndID["applist"]["apps"][i]["name"] : [{
-            'reviews' : [{
-            }]
-        }]
+    dataset.update({
+        appNameAndID["applist"]["apps"][i]["name"] : appNameAndID["applist"]["apps"][i]["appid"]
     })
+    reviewList["titles"].update({
+        appNameAndID["applist"]["apps"][i]["name"] : {
+            'reviews' : [
+            ]
+        }
+    })
+
 #Get all game reviews for all games in our dataset
 getAllGameReviews(dataset)
+
+with open('data.csv', w) as f:
+    w = csv.writer(f)
+    w.writerows(reviewList.items())

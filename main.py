b80a8de57cbb092a6fd0cec08ccf12ba1b5c4fca
Charlie Chin <cchin013@ucr.edu>
2018-06-29 20:07:59 -0700

    columns of data now properly append to the main dataframe

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from pyspark.sql import SparkSession
 response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
 appNameAndID = response.json()
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
 #key: game name, value: game's app id, e.g. dataset["Counter-Strike"] returns 10 (its appid)
 gameIDMapping = {}
@@ -77,11 +77,11 @@ for i in range(len(appNameAndID["applist"]["apps"])):
     for j in range(0, reviewsResponse["query_summary"]["total_reviews"], 20):
         # Go through all reviews for the game in batches of 20
         # Place them in the proper location in our reviewList dataset based on the game
-        for k in range(len(reviewsResponse["reviews"])):
+        for review in reviewsResponse["reviews"]:
             # debugging statement to check if we are really getting reviews
             # print(reviewsResponse["reviews"][k]["review"])
             reviewList["titles"][appNameAndID["applist"]["apps"][i]["name"]]["reviews"].append({
-                reviewsResponse["reviews"][k]["review"]
+                review["review"]
             })
         # Get the next batch of 20, e.g. first we get reviews 0-20, then 21-40, etc..
         parameters["start_offset"] += 20
@@ -95,6 +95,8 @@ for i in range(len(appNameAndID["applist"]["apps"])):
             continue
         reviewsResponse = response.json()
         print(str(j) + " reviews done")
-    data[appNameAndID["applist"]["apps"][i]["name"]] = pd.Series(reviewList["titles"][appNameAndID["applist"]["apps"][i]["name"]]["reviews"])
+    temp = data
+    newData = pd.DataFrame({appNameAndID["applist"]["apps"][i]["name"] : reviewList["titles"][appNameAndID["applist"]["apps"][i]["name"]]["reviews"]})
+    data = pd.concat([temp, newData], axis=1)
     data.to_csv('data.csv', encoding='utf-8')
     data.reset_index()

a57d9c01dbcf20589f8955e5600529599ff43565
Charlie Chin <cchin013@ucr.edu>
2018-07-02 00:09:54 -0700

    switched data storage to SQLite3, pandas too slow on large CSVs

--- main.py
+++ main.py
@@ -19,17 +19,20 @@ from bs4 import BeautifulSoup as bSoup
 # For extracting relevant information from the data
 from pyspark.sql import SparkSession
 
-# site = 'https://stackoverflow.com/questions/28610508/how-to-get-raw-html-text-of-a-given-url-using-python'
-# htmlSite = urllib.request.urlopen(site)
-# soup = bSoup(htmlSite, 'html.parser')
-# print(soup)
+# For data management
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+crsr = connection.cursor()
+crsr.execute('''CREATE TABLE reviews (
+review_id INTEGER PRIMARY KEY,
+game_name TEXT,
+review TEXT,
+time_created INTEGER)''')
 
 #Get a list of all games on Steam
 response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
 appNameAndID = response.json()
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.width', 1000)
 #key: game name, value: game's app id, e.g. dataset["Counter-Strike"] returns 10 (its appid)
 gameIDMapping = {}
 #key: "titles"->game name->reviews, value: all reviews for that game in an arbitrary order
@@ -45,17 +48,15 @@ for i in range(len(appNameAndID["applist"]["apps"])):
     reviewList["titles"].update({
         appNameAndID["applist"]["apps"][i]["name"] : {
             'reviews' : [
-            ],
-            'dates' : [
-            ]
+            ]#,
+            #'dates' : [
+            #]
         }
     })
 
 # Get all game reviews for all games in our dataset
 # Steam API parameters for what we want: all recent reviews(including positive and negative) in English in the last 10 days, purchased from anywhere
 # More info here: https://partner.steamgames.com/doc/store/getreviews
-data = pd.DataFrame()
-dates = pd.DataFrame()
 parameters = {'filter': 'recent', 'language': 'english', 'day_range': '10', 'start_offset': '0',
               'review_type': 'all',
               'purchase_type': 'all',
@@ -75,20 +76,19 @@ for app in appNameAndID["applist"]["apps"]:
     # If the query is empty, skip that game/app
     if reviewsResponse["success"] == 2:
         continue
+    if reviewsResponse["query_summary"]["total_reviews"] == 0:
+        continue
     # print("_____________________________THE FOLLOWING IS FOR THE GAME: " + str(dataset["games"][i]["title"]))
     # Go through all the reviews for a given game
     for j in range(0, reviewsResponse["query_summary"]["total_reviews"], 20):
         # Go through all reviews for the game in batches of 20
         # Place them in the proper location in our reviewList dataset based on the game
         for review in reviewsResponse["reviews"]:
-            # debugging statement to check if we are really getting reviews
-            # print(reviewsResponse["reviews"][k]["review"])
             reviewList["titles"][app["name"]]["reviews"].append({
                 review["review"]
             })
-            reviewList["titles"][app["name"]]["dates"].append({
-                review["timestamp_created"]
-            })
+            crsr.execute("INSERT OR REPLACE INTO reviews VALUES (NULL, ?, ?, ?)", (app["name"], review["review"], review["timestamp_created"]))
+            connection.commit()
         # Get the next batch of 20, e.g. first we get reviews 0-20, then 21-40, etc..
         parameters["start_offset"] += 20
         try:
@@ -100,18 +100,5 @@ for app in appNameAndID["applist"]["apps"]:
             print(e)
             continue
         reviewsResponse = response.json()
-        print(str(j) + " reviews done")
-    temp = data
-    temp2 = dates
-    #Get new data/dates
-    newData = pd.DataFrame({app["name"] : reviewList["titles"][app["name"]]["reviews"]})
-    newDates = pd.DataFrame({app["name"] : reviewList["titles"][app["name"]]["dates"]})
-    #Add them to current data/dates dataframes
-    data = pd.concat([temp, newData], axis=1)
-    dates = pd.concat([temp2, newDates], axis=1)
-    #Write them to the file
-    data.to_csv('data.csv', encoding='utf-8')
-    dates.to_csv('dates.csv', encoding='utf-8')
-    #Reset index
-    data.reset_index()
-    dates.reset_index()
+        print(str(j) + " reviews done for game " + app["name"])
+connection.close()
